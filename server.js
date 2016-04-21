a6984d30522cc118ab21148b854575fbada8ef9c
Shawn Saras <shawnsaras@gmail.com>
2016-04-21 16:58:05 -0700

    port settings 3

--- server.js
+++ server.js
@@ -1,9 +1,33 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
+// var connect = require('connect');
+// var serveStatic = require('serve-static');
 
-// var port = normalizePort(process.env.PORT || '3000');
-var port = 8080;
+// // var port = normalizePort(process.env.PORT || '3000');
+// var port = 8080;
 
-connect().use(serveStatic(__dirname)).listen(port, function(){
-    console.log('Server running on: ' + port);
+// connect().use(serveStatic(__dirname)).listen(port, function(){
+//     console.log('Server running on: ' + port);
+// });
+
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+// app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+  // ejs render automatically looks in the views folder
+  // res.render('index');
+});
+
+app.listen(port, function() {
+  console.log('Our app is running on http://localhost:' + port);
 });
