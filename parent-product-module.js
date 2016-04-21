690cefb117381c924499f0d4b1d54ac68d49de04
Shawn Saras <shawnsaras@gmail.com>
2016-04-21 15:30:16 -0700

    added gitignore - ignoring node_modulesw

--- parent-product-module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var app = angular.module('parent-product-module', [
-    'product-descriptions-module',
-    'product-reviews-module',
-    'product-specs-module',
-    'product-tabs-module',
-    'product-gallery-module'
-  ]);
-})();
